c0661fdddcac61f2e6595496ab1ec6a26ad2c52d
accidentalrebel <accidentalrebel@gmail.com>
2019-07-13 23:10:52 +0800

    Renamed the powerkit functions.

--- powerkit.lua
+++ powerkit.lua
@@ -1,17 +1,17 @@
-function rbl_fflr(num)
+function fflr(num)
    return flr(num*100)/100
 end
 
-function rbl_fceil(num)
+function fceil(num)
    return ceil(num*100)/100
 end
 
-function rbl_assert(cond,message)
+function assert(cond,message)
    if not cond then
       rbl_error(message)
    end
 end
 
-function rbl_error(message)
+function error(message)
    printh(">>>>>>> ERROR: "..message)
 end
