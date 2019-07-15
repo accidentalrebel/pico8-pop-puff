a79a753766ef189d7f2e913170eb006f64ba292f
accidentalrebel <accidentalrebel@gmail.com>
2019-07-15 23:56:29 +0800

    Can now generate acceptable.

--- tools/level-converter.py
+++ tools/level-converter.py
@@ -1,8 +1,9 @@
 char_array = [ '-', '0', 'P', 'F', 'B', '^', '>', 'v', '<', 's', '!', ']', ';', '[', 'x' ]
+alpha_array = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ]
 
-def open_file():
+def open_file(level_string):
     map_string = ''
-    with open('test-1.txt','r') as f:
+    with open(level_string + '.txt','r') as f:
         lines = []
         line = f.readline()
         lines_count = 0
@@ -32,17 +33,38 @@ def pad_map_data(map_data):
     return map_data
 
 def convert_map_data(map_data):
+    string = ''
+    a_count = 0
     for data in map_data:
         if data == None or len(data) <= 0:
             break
         
         first = data[0]
+        if first == '-':
+            first = 0
+        else:
+            first = int(first)
+        
+        if first > 0:
+            string += str(first)
+
         second = data[1]
-        print('Index: ' + second + ': ' + str(char_array.index(second)))
+        second = char_array.index(second)
+
+        if second == 0:
+            a_count += 1
+        else:
+            if a_count > 0:
+                string += str(a_count)
+                
+            string += alpha_array[second]
+            a_count = 0
+
+    print(string)
     
     return
 
-map_string = open_file()
+map_string = open_file('test-1')
 if map_string == '':
     sys.exit()
         
